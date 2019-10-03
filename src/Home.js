4f237a46594f65373d0f489ef1637810f6763f57
Richard Wong <wong.richardh@gmail.com>
2019-10-03 01:22:51 -0700

    Install react photo gallery

--- src/Home.js
+++ src/Home.js
@@ -3,20 +3,6 @@ import { LandingPage } from './components/LandingPage'
 
 export const Home = () => {
     return (
-//         <div>
-//             <h2>Home</h2>
-//             <p> Thu 2 May 2019 - Filed under:
-
-// One weekend in March 2019 Kelly Link and Ursula Grant put together four zines. They are all saddle-stitched (i.e. stapled), with b&w covers and interiors and card color covers and were printed by the fine people of Paradise Copies. We also made sets of b&w postcards of Ursula’s illustrations for Kelly’s Horoscope Stories. We sold the chapbooks and gave away postcards at the AWP and Juniper book fairs (so many Pisces and Taurus!). We’ll have them at WisCon and the Brooklyn Book Fair and other fairs and festivals we go to and now we’re making them available here. The zines can be ordered individually or in bundles with or without the postcards. The postcards are being sold in sets of a dozen (mixed or you can choose a dozen of a singe sign) and we’ll have continue to have them as giveaways at book fairs, &c.
-
-// Zines will be shipped by First Class Mail without tracking — unless Priority Mail is added — in the USA. (99% of orders arrive ok but if you order more than say a dozen zines, it may be worth it.) International shipping will be added by Paypal or by email once your order is placed.
-
-// Tip: if you’d like multiple copies, select that here as selecting multiple copies on the PayPal page will not include the tiny discounts.
-
-// Click the images to see larger photos of the zines on our kitchen floor!</p>
-            
-//         </div>
-
         <LandingPage/>
     )
 }
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import {Image} from 'react-bootstrap';
 import styled from 'styled-components';
+import Gallery from 'react-photo-gallery'
 
 import pic1 from '../assets/examplePic1.jpg';
 import pic2 from '../assets/examplePic2.jpg';
@@ -20,18 +21,45 @@ const Styles = styled.div`
     }
 `;
 
+export const photos = [
+    // {
+    //   src: "https://source.unsplash.com/2ShvY8Lf6l0/800x599",
+    //   width: 4,
+    //   height: 3
+    // },
+    // {
+    //   src: "https://source.unsplash.com/Dm-qxdynoEc/800x799",
+    //   width: 1,
+    //   height: 1
+    // },
+    {
+      src: "https://source.unsplash.com/qDkso9nvCg0/600x799",
+      width: 3,
+      height: 4
+    },
+    {
+      src: "https://source.unsplash.com/iecJiKe_RNg/600x799",
+      width: 3,
+      height: 4
+    },
+  ];
+
 export const LandingPage = () => (
 
-    <Styles>
-        <Container>
-            <Row>
-                <Col sm={12} md={4} lg={4}> <Image src= {pic2}/> </Col>
-                <Col sm={12} md={4} lg={4}> <Image src= {pic3}/> </Col>
-                <Col sm={12} md={4} lg={4}> <Image src= {pic1}/> </Col>
+    // <Styles>
+    //     <Container>
+    //         <Row>
+    //             <Col sm={12} md={4} lg={4}> <Image src= {pic2}/> </Col>
+    //             <Col sm={12} md={4} lg={4}> <Image src= {pic3}/> </Col>
+    //             <Col sm={12} md={4} lg={4}> <Image src= {pic1}/> </Col>
 
-            </Row>
-        </Container>
-    </Styles>
+    //         </Row>
+    //     </Container>
+    // </Styles>
+
+    <Gallery photos={photos} direction={"column"}> 
+
+    </Gallery>
 
 
 )
