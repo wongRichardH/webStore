96d7bcf035128157896e8f01f1e980bbb4dc511a
Richard Wong <wong.richardh@gmail.com>
2019-11-25 23:48:52 -0800

    Make formatting for responsive grid

--- src/components/Shop/ItemDetails/ThumbnailGrid.js
+++ src/components/Shop/ItemDetails/ThumbnailGrid.js
@@ -1,26 +1,41 @@
 import React  from 'react'
 import styled from 'styled-components';
 
+
+const GridImage = styled.img`
+    /* max-width: 25%; */
+
+    /* width: 100px; */
+
+    max-width:100px;
+    max-height:100px;
+    width:auto;
+    height:auto;
+`
+
+const StyledGrid = styled.div`
+    display:flex;
+    flex-wrap: wrap; 
+    justify-content: center;
+    background-color: red;
+
+    margin: 3px;
+`
+
 const ThumbnailGrid = (props) => {
 
     console.log(props)
 
     let productImages = props.productImages.map(eachItem => {
         return (
-            <div>
-                <img src={eachItem} alt="noImage" width="5" height="5"/>
-            </div>
+            <StyledGrid>
+                <GridImage src={eachItem} alt="noImage"/>
+            </StyledGrid>
         )
     })
 
     return (
-
-        // {productImages}
-        
-        <div>
-            GRid goes heres
-        </div>
-
+        <StyledGrid>{productImages}</StyledGrid>
     )
 }
 
